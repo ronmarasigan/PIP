8980249e608dc232efe94a7f6b2e08b973f21a0b
gilbitron <drummermanny@gmail.com>
2011-04-04 16:26:03 +0100

    Fixing Exmaple_model.

--- application/models/example_model.php
+++ application/models/example_model.php
@@ -5,8 +5,8 @@ class Example_model extends Model {
 	public function getSomething($id)
 	{
 		$id = $this->escapeString($id);
-		$class = $this->query('SELECT * FROM something WHERE id="'. $id .'"');
-		return $class;
+		$result = $this->query('SELECT * FROM something WHERE id="'. $id .'"');
+		return $result;
 	}
 
 }
