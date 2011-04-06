2fb61c7180c351c5548d0636f3bcda40134588fb
Gilbert Pellegrom <drummermanny@gmail.com>
2011-04-06 10:13:05 +0100

    Created folder structure for static content. Moved CSS file from application/view folder to static/css Added .gitignore files to preserve folder structure. Added BASE_URL link to CSS in header.php. Removed CSS styles.

--- application/views/header.php
+++ application/views/header.php
@@ -7,6 +7,6 @@
     <meta name="description" content="">
     <meta name="author" content="">
     
-    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/style.css" type="text/css" media="screen" />
 </head>
 <body>
