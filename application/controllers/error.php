fdfb32882345e87526130b8d0e10884c450cf5ca
Maciel Sousa <macielcr7@gmail.com>
2012-12-25 18:27:22 -0200

    Update application/controllers/error.php

--- application/controllers/error.php
+++ application/controllers/error.php
@@ -2,15 +2,30 @@
 
 class Error extends Controller {
 	
-	function index()
-	{
-		$this->error404();
+	function index($segments){
+		return $this->error($this->getMessage($segments));
 	}
-	
-	function error404()
-	{
-		echo '<h1>404 Error</h1>';
-		echo '<p>Looks like this page doesn\'t exist</p>';
+
+	function getMessage($segments){
+		if(is_array($segments)){
+			$segments = $segments[0];
+		}
+
+		return $segments;
+	}
+
+	function error($segments="Error!"){
+		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+			return json_encode(array(
+				'success' => false, 
+				'data' => array(),
+				'msg'=> utf8_encode($segments)
+			));
+		}
+		else{
+			return utf8_decode("<h1>{$segments}</h1>");
+		}
+		
 	}
     
 }
