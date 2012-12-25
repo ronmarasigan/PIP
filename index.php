bcbc5633a1a973ab1e87e2dc358de68ed2b015de
Maciel Sousa <macielcr7@gmail.com>
2012-12-25 18:23:09 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
 /*
- * PIP v0.5.3
+ * PIP v0.5.4
  */
 
 //Start the Session
 session_start(); 
 
 // Defines
-define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
-define('APP_DIR', ROOT_DIR .'application/');
+define('DS', DIRECTORY_SEPARATOR);
+
+define('ROOT_DIR', realpath(dirname(__FILE__)) . DS);
+define('APP_DIR', ROOT_DIR .'application' . DS);
 
 // Includes
-require(APP_DIR .'config/config.php');
-require(ROOT_DIR .'system/model.php');
-require(ROOT_DIR .'system/view.php');
-require(ROOT_DIR .'system/controller.php');
-require(ROOT_DIR .'system/pip.php');
+require(APP_DIR  .'config'. DS .'config.php');
+require(ROOT_DIR .'system'. DS .'model.php');
+require(ROOT_DIR .'system'. DS .'view.php');
+require(ROOT_DIR .'system'. DS .'controller.php');
+require(ROOT_DIR .'system'. DS .'application.php');
 
 // Define base URL
 global $config;
