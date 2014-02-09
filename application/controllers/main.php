6a2ffae5894314c79afbe7c5d0527d7235e79817
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2014-02-09 23:58:24 +0200

    Updated ASSETS loader

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,13 +1,21 @@
 <?php
 
 class Main extends Controller {
-	
+
 	function index()
 	{
 		$template = $this->loadView('main_view');
+    $template->setCSS(array(
+      array("static/css/style.css", "intern"),
+      array("http://www.example.com/default.css", "extern")
+    ));
+    $template->setJS(array(
+      array("static/js/index.js", "intern"),
+      array("http://www.example.com/static.js", "extern")
+    ));
 		$template->render();
 	}
-    
+
 }
 
 ?>
