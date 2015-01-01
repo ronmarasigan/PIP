7f47e20546a08051eb55bb25e39cb1e475d92780
Julian Cizmic <j5@j5.io>
2014-12-31 16:38:23 -0800

    Update the "first run" page
    The page now has a working link to the documentation. Basic styling
    was applied, and a link to the project's GitHub page was added.

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,10 +1,50 @@
 <?php include('header.php'); ?>
 	
+	<style type="text/css">
+	<!--
+
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	body {
+		font-family: Verdana;
+	}
+
+	h1 {
+		border-left: 20px solid #AAA;
+		font-size: 20px;
+		padding-left: 10px;
+	}
+
+	ul {
+		list-style-type: none;
+		margin-left: 32px;
+	}
+
+	li:before {
+		content: '\2192';
+		margin-right: 10px;
+	}
+
+	#content {
+		border: 1px dotted #AAA;
+		margin: 100px auto;
+		padding: 15px;
+		width: 400px;
+	}
+
+	-->
+	</style>
+
     <div id="content">
         
-        <h1>Welcome to PIP</h1>
-        <p>To get started please read the documentation at <a href="http://pip.dev7studios.com/">http://pip.dev7studios.com</a>.</p>
-        
+        <h1>Welcome to PIP.</h1>
+       	<ul>
+        	<li><a href="http://gilbitron.github.io/PIP/">Documentation</a></li>
+        	<li><a href="https://github.com/gilbitron/PIP">GitHub</a></li>
+        </ul>
     </div>
 
 <?php include('footer.php'); ?>
