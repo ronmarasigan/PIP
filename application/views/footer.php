6c9575e29d79e922ccab743f0123ee04307e9130
PC <pravin.innocent@gmail.com>
2015-01-02 20:19:01 +0530

    adding js components and css

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,2 +1,10 @@
+<div style="min-height:4000px"> s </div>
 </body>
+
+<script type="text/javascript" src="<?php echo Config::$URL; ?>static/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo Config::$URL; ?>static/js/components/scrollToView.js"></script>
+<script type="text/javascript" src="<?php echo Config::$URL; ?>static/js/components/ui.js"></script>
+<script type="text/javascript">
+QS.ui.goTop.init();
+</script>
 </html>
