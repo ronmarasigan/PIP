19e221610687d0795a5e8533ddc4be98a20b918d
Dushan <dushankw@gmail.com>
2015-06-16 10:00:19 +0000

    TEST PENDING: session security stuff

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
-    session_start(); 
-
+    // Base paths
     define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
     define('APP_DIR', ROOT_DIR .'application/');
-
     require(ROOT_DIR .'system/config.php');
-    require(ROOT_DIR .'system/model.php');
-    require(ROOT_DIR .'system/view.php');
-    require(ROOT_DIR .'system/controller.php');
-    require(ROOT_DIR .'system/pip.php');
-
+    
+    // Settings 
     global $config;
     define('BASE_URL', $config['base_url']);
+
+    // Secure session
+    session_name($config['session_name']);
+    session_set_cookie_params($lifetime = $config['cookie_lifetime'], $secure = $config['https_cookie'], $http_only = $config['http_only']);
+    session_start(); 
+    
+    // Set variable for tracking the number of requests per session id
+    if(!isset($_SESSION['regen'])) {
+        $_SESSION['regen'] = 0;
+    }
+    
+    // Rotate session id every N requests to protect from session fixation
+    if(++$_SESSION['regen'] > $config['rotation_interval']) {
+        $_SESSION['regen'] = 0;
+        session_regenerate_id(true);
+    }     
     
     // PHP settings for dev mode 
     if(!$config['production']) {
@@ -20,6 +31,12 @@
         ini_set('memory_limit', '-1');
         set_time_limit(0);
     }
+
+    // Base classes for application
+    require(ROOT_DIR .'system/model.php');
+    require(ROOT_DIR .'system/view.php');
+    require(ROOT_DIR .'system/controller.php');
+    require(ROOT_DIR .'system/pip.php');
 
     // Call PIP
     pip();
--- system/config.php
+++ system/config.php
@@ -1,12 +1,22 @@
 <?php 
     // Mode
     $config['production'] = false;
+    
+    // Session rotation interval
+    $config['rotation_interval'] = 20;    
+    
+    // Session cookie settings
+    $config['session_name'] = 'pip'; // Change me
+    $config['http_only'] = true; // You really shouldn't change this
+    $config['cookie_lifetime'] = 3600; // 1 hour in seconds
 
     // URL
     if((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
         $config['base_url'] = 'https://'.$_SERVER['HTTP_HOST'].'/';
+        $config['https_cookie'] = true;
     } else {
         $config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
+        $config['https_cookie'] = false;
     }
 
     // Database credentials and default/permitted controllers
