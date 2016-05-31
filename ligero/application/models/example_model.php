379b4c53e657d1a0c83052ae6cfef30a9a297512
acap1831 <acap1831@infpgm12d.asociacioncibao.com.do>
2016-05-31 13:16:53 -0400

    buu

--- ligero/application/models/example_model.php
+++ ligero/application/models/example_model.php
@@ -10,9 +10,9 @@ class Example_model extends Model {
 	}
         public function prueba($id)
 	{       
-		$this->execute('CREATE TABLE people (full_name varchar(255), job_title varchar (255))');
+		//$this->execute('CREATE TABLE people (full_name varchar(255), job_title varchar (255))');
                 echo "Table people has been created \n";
-                $this->execute('INSERT INTO people (full_name, job_title) VALUES ("John Doe","manager")');
+             //   $this->execute('INSERT INTO people (full_name, job_title) VALUES ("John Doe","manager")');
                 echo "Row inserted \n";
 		//$result = $this->query('SELECT * FROM people WHERE id="'. $id .'"');
 		return $result;
