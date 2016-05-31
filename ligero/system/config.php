ba0930d1674829844d74765bcb6dacbd34e3c95f
CesarDarinel <cesardarinelortiz@gmail.com>
2016-05-31 12:13:50 -0400

    Update config.php

--- ligero/system/config.php
+++ ligero/system/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-$config['base_url'] = 'http://localhost:8000/'; // Base URL including trailing slash (e.g. http://localhost/)
+$config['base_url'] = 'https://ligero.herokuapp.com/'; // Base URL including trailing slash (e.g. http://localhost/)
 
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
