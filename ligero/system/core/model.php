a38a8bec38f8eeed92b63dd04b983dfeff357d4c
acap1831 <acap1831@infpgm12d.asociacioncibao.com.do>
2016-05-31 16:27:37 -0400

    eliminando errores con heroku

--- ligero/system/core/model.php
+++ ligero/system/core/model.php
@@ -16,9 +16,7 @@ class Model {
             $this->connection = new PDO(ROOT_DIR . 'ligero/system/core/temp/db/' . $config['db_name'] . '.db') or die('Error con sqlite, Exploto :D' . PDOException);
         } elseif ($config['db_Database'] == 'pgsql') {
             try {
-                 $this->connection = new PDO("pgsql:dbname=".$config['db_name'].
-                                             ";port=5432;host=".$config['db_host']."",$db_username, $db_password); 
-                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                 $this->connection = new PDO("pgsql:dbname=".$config['db_name'].";port=5432;host=".$config['db_host']."",$db_username, $db_password); 
             } catch (PDOException $e) {
                 echo 'Falló la conexión: ' . $e->getMessage();
             }
